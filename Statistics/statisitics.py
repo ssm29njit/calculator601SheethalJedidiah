1caab9d73899806b007098b56de2916a46fb0766
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 03:02:28 -0500

    Feature - Mean and Random0 Mean under Statistics

--- Statistics/statisitics.py
+++ Statistics/statisitics.py
@@ -1,7 +1,13 @@
 from Calculator.Calculator import Calculator
+from Statistics.Mean import mean
+from Statistics.randomMean import random_mean
 
 class Statistics(Calculator):
-    data = []
 
-    def __init__(self):
-        super().__init__()
+    def mean(self, data):
+        self.result = mean(data)
+        return self.result
+
+    def randomMean(self, data):
+            self.result = random_mean(data)
+            return self.result
