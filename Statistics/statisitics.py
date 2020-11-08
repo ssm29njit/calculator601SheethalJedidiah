12d8c4fa98cb0b03e515ac2a6eb9fe51b8cc884a
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 02:58:14 -0500

    bugFix

--- Statistics/statisitics.py
+++ Statistics/statisitics.py
@@ -1,7 +1,10 @@
 from Calculator.Calculator import Calculator
+from Statistics.Mean import mean
+
+
 
 class Statistics(Calculator):
-    data = []
 
-    def __init__(self):
-        super().__init__()
+    def mean(self, data):
+        self.result = mean(data)
+        return self.result
