16290e29d078e5ebf918784514dc49790dbf9eb6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 00:22:47 -0500

    bugFix -setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='Calculator',
       version='0.0.1',
       description='It is a calculator',
       author='Sheethal Mathew and Jedidiah John Nagarajan',
-      author_email= 'ssm29@njit.edu, ',
+      author_email= 'ssm29@njit.edu ',
       url='https://github.com/ssm29njit/calculator601SheethalJedidiah.git',
       packages=find_packages(),
       )
